7c61d0f0431abc6088ea51a22cb02bf2cd2c886e
Matt Arnold <matt@Gerrard.local>
2018-10-27 21:08:38 -0500

    remove @ from non-mentions

--- app.py
+++ app.py
@@ -42,6 +42,9 @@ def handle_ploosploos(matches, channel):
     r = redis.from_url(os.getenv('REDIS_URL'))
     for match in matches:
         thing, plusminus = match
+        if thing.startswith('@'):
+            thing = thing.lstrip('@')
+
         print(f'do a {plusminus} on {thing}')
         if plusminus == '++':
             new_val = r.incr(thing)
