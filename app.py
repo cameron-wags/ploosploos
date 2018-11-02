5a63d6bf4b411572df8f14fdfbaee9ca23882dbd
Cameron <cam.wagstaff@gmail.com>
2018-11-01 21:43:26 -0400

    Add draft for scores functionality

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ import random
 
 app = Flask(__name__)
 THE_REGEX = re.compile(r'(<?@[\w:+-]+>?) *([+]{2}|[-]{2})')
+LEADERBROAD_REXEG = re.compile(r'/ploosploos (scores?|leaderboard|count)')
 SLAKC_TOKEN = os.getenv('SALCK_TOKEN')
 CLASK_URL = 'https://slack.com/api/chat.postMessage'
 # It actually doesn't matter if someone ++'s this, they'd have to be fast.
@@ -36,10 +37,38 @@ def main_thingy():
         t = Thread(group=None, target=handle_ploosploos, args=(matches, channel, event_id))
         t.start()
         print('yes!')
+    elif LEADERBROAD_REXEG.match(thing['event']['text']):
+        t = Thread(group=None, target=handle_leaderboard, args=(channel, event_id))
+        t.start()
     else:
         print('no')
 
     return ''
+
+def handle_leaderboard(channel, event_id):
+    r = redis.from_url(os.getenv('REDIS_URL'))
+
+    if event_id == r.get(SOMETHING_NO_ONE_WILL_EVER_SAY):
+        pass
+    r.set(SOMETHING_NO_ONE_WILL_EVER_SAY, event_id)
+
+    # todo get a summary of all numeric keys from redis, however that happens
+    message = f'{uncool_synonym().capitalize()}, Cameron and Matt haven\'t made a leaderboard yet!'
+
+    body = {
+            'token': SLAKC_TOKEN,
+            'channel': channel,
+            'text': message,
+    }
+
+    headers = {
+            'content-type': 'application/json',
+            'authorization': f'Bearer {SLAKC_TOKEN}',
+    }
+
+    response = requests.post(CLASK_URL, headers=headers, json=body)
+    response.raise_for_status()
+
 
 def handle_ploosploos(matches, channel, event_id):
     r = redis.from_url(os.getenv('REDIS_URL'))
